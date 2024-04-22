e7cbb744ba5eb5d20d828b2ded9cf6b221f68f0a
karanpal97 <karangill1810@gmail.com>
2024-04-22 15:01:32 +0530

    added the types

--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Room } from './entities/room.entity';
 import { Repository } from 'typeorm';
+import { RoomStatus } from './entities/room.entity';
+import { apiResponse } from 'src/type/apiResponse';
 
 @Injectable()
 export class RoomService {
@@ -15,12 +17,28 @@ export class RoomService {
     return await this.roomRepo.save(createRoomDto);
   }
 
-  findAll() {
-    return `This action returns all room`;
+  async findAll(): Promise<apiResponse> {
+    try {
+      const rooms = await this.roomRepo.find({
+        where: { status: RoomStatus.AVAILABLE },
+      });
+      return {
+        message: 'successfully get all the rooms',
+        success: true,
+        data: rooms,
+      };
+    } catch (error) {
+      throw new HttpException('no room is available', HttpStatus.NOT_FOUND);
+    }
   }
 
   async findOne(id: number) {
-    return await this.roomRepo.findOneBy({ room_id: id });
+    try {
+      return await this.roomRepo.findOneBy({ room_id: id });
+      // return { message: 'successfully get the room', room };
+    } catch (error) {
+      throw new HttpException('room is not availble', HttpStatus.NOT_FOUND);
+    }
   }
 
   update(id: number, updateRoomDto: UpdateRoomDto) {
